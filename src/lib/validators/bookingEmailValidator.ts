f8625a3f401ef3ec11f74300631f9af3289d6723
himanshu7437 <himanshujangra368@gmail.com>
2026-04-07 01:31:39 +0530

    remove home page unused components

--- src/lib/validators/bookingEmailValidator.ts
+++ src/lib/validators/bookingEmailValidator.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const bookingEmailValidator = z.object({
-  userEmail: z.string().email("Invalid email format"),
+  userEmail: z.email("Invalid email format"),
   userName: z.string().min(2, "Name must be at least 2 characters"),
   facility: z.string().min(1, "Facility is required"),
   checkIn: z.string().or(z.date()),
